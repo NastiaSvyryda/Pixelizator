14bfb8743930a62896c06794bcc39e9d06d4bfe7
asvyryda <asvyryda@student.ucode.world>
2020-10-20 19:17:54 +0300

    image load

--- src/main/java/MyServlet.java
+++ src/main/java/MyServlet.java
@@ -1,18 +1,99 @@
+import javax.imageio.ImageIO;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.MultipartConfig;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.Part;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.awt.image.Raster;
+import java.awt.image.WritableRaster;
+import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
 
 @WebServlet(name = "MyServlet")
+@MultipartConfig(fileSizeThreshold=1024*1024*2, // 2MB
+        maxFileSize=1024*1024*10,      // 10MB
+        maxRequestSize=1024*1024*50)   // 50MB
 public class MyServlet extends HttpServlet {
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
+    private static final String SAVE_DIR = "uploadFiles";
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("Enter doPost");
+        String fileName = null;
+        // gets absolute path of the web application
+        String appPath = request.getServletContext().getRealPath("");
+        // constructs path of the directory to save uploaded file
+        String savePath = appPath + File.separator + SAVE_DIR;
+        // creates the save directory if it does not exists
+        File fileSaveDir = new File(appPath);
+        if (!fileSaveDir.exists()) {
+            fileSaveDir.mkdir();
+        }
+
+        for (Part part : request.getParts()) {
+            fileName = extractFileName(part);
+            // refines the fileName in case it is an absolute path
+            fileName = new File(fileName).getName();
+            part.write(savePath + File.separator + fileName);
+        }
+
+        final int PIX_SIZE = 10;
+
+// Read the file as an Image
+        BufferedImage img = ImageIO.read(new File(savePath + File.separator + fileName));
+
+// Get the raster data (array of pixels)
+        Raster src = img.getData();
+
+// Create an identically-sized output raster
+        WritableRaster dest = src.createCompatibleWritableRaster();
+
+// Loop through every PIX_SIZE pixels, in both x and y directions
+        for(int y = 0; y < src.getHeight(); y += PIX_SIZE) {
+            for(int x = 0; x < src.getWidth(); x += PIX_SIZE) {
+
+                // Copy the pixel
+                double[] pixel = new double[3];
+                pixel = src.getPixel(x, y, pixel);
+
+                // "Paste" the pixel onto the surrounding PIX_SIZE by PIX_SIZE neighbors
+                // Also make sure that our loop never goes outside the bounds of the image
+                for(int yd = y; (yd < y + PIX_SIZE) && (yd < dest.getHeight()); yd++) {
+                    for(int xd = x; (xd < x + PIX_SIZE) && (xd < dest.getWidth()); xd++) {
+                        dest.setPixel(xd, yd, pixel);
+                    }
+                }
+            }
+        }
+
+// Save the raster back to the Image
+        img.setData(dest);
+
+// Write the new file
+        ImageIO.write(img, "jpg", new File(savePath  + File.separator + "px_"+ fileName));
+        request.setAttribute("path", SAVE_DIR + File.separator + "px_" + fileName);
+        getServletContext().getRequestDispatcher("/index.jsp").forward(
+                request, response);
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("Enter doGet");
+        request.getRequestDispatcher("index.jsp").forward(request, response);
+    }
 
+    private String extractFileName(Part part) {
+        String contentDisp = part.getHeader("content-disposition");
+        String[] items = contentDisp.split(";");
+        for (String s : items) {
+            if (s.trim().startsWith("filename")) {
+                return s.substring(s.indexOf("=") + 2, s.length()-1);
+            }
+        }
+        return "";
     }
 }
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -8,45 +8,75 @@
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
 <html>
 <head>
-    <title>Hello, I am a Java web app!</title>
+    <meta name="description" content="Pixelizator">
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <%--    через 5 секунд переходит на страницу    --%>
+    <%--        <meta http-equiv="refresh" content="5; URL=http://www.google.com">--%>
+    <title>Pizelizator</title>
 </head>
 <body>
 <h1>Simple Java Web App Demo</h1>
 <script>
     function updateSize() {
         var nBytes = 0,
-            oFiles = document.querySelector('input[type="file"]').files,
+            oFiles = document.getElementById("file").files,
             nFiles = oFiles.length;
         for (var nFileId = 0; nFileId < nFiles; nFileId++) {
             nBytes += oFiles[nFileId].size;
         }
-        var sOutput = (nBytes/1000000).toFixed(3) + "MiB";
-        document.getElementById("fileSize").innerHTML = sOutput;
+        document.getElementById("fileSize").innerHTML = (nBytes / 1000000).toFixed(3) + "MiB";
     }
 </script>
-<input id="file" type='file' onchange=updateSize() />
-<br><img id="myImg" src="#" alt="your image" height=600 width=800>
+
+<%--<form method="post" enctype="multipart/form-data">--%>
+<%--    Name: <input type="text" name="submit"/> <br/>--%>
+<%--    <button type="submit">send</button>--%>
+<%--<input id="file" type='file' name="photo" onchange=updateSize() />--%>
+<%--<br><img id="myImg" src="#" alt="your image" height=600 width=800>--%>
+<%--</form>--%>
+
+<form method="post" enctype="multipart/form-data" action="Pixelizator">
+    <input name="image" id="file" type="file" accept="image/*" onchange=updateSize()>
+    <br>
+    <button type="submit">send</button>
+</form>
+
 <p>
     total size: <span id="fileSize">0</span>
 </p>
-<script type="text/javascript">
-    window.addEventListener('load', function() {
-        document.querySelector('input[type="file"]').addEventListener('change', function() {
-            if (this.files && this.files[0]) {
-                var img = document.querySelector('img');  // $('img')[0]
-                img.src = URL.createObjectURL(this.files[0]); // set src to blob url
-                img.onload = imageIsLoaded;
-                
-            }
-        });
-    });
 
-    function imageIsLoaded() {
-        alert(this.src);  // blob url
-        // update width and height ...
+<div class="row"><span id="output"></span></div>
+
+<script>
+    function showImage(evt) {
+        var file = evt.target.files; // FileList object
+        var f = file[0]
+        // Only process image files.
+        if (!f.type.match('image.*')) {
+            alert("Только изображения....");
+        }
+        var reader = new FileReader();
+        // Closure to capture the file information.
+        reader.onload = (function(theFile) {
+            return function(e) {
+                // Render thumbnail.
+                var span = document.createElement('span');
+                span.innerHTML = ['<img class="img-thumbnail" src="', e.target.result,
+                    '" title="', escape(theFile.name), '"/>'].join('');
+                document.getElementById('output').innerHTML = "";
+                document.getElementById('output').insertBefore(span, null);
+            };
+        })(f);
+        // Read in the image file as a data URL.
+        reader.readAsDataURL(f);
     }
+    function reset(evt) {
+        var file = evt.target.files; // FileList object
+        var f = file[0];
+        f = "";
+    }
+    document.getElementById('file').addEventListener('change', showImage, false);
+    // document.getElementById('file').addEventListener('load', reset, false);
 </script>
-
-<%--<p>To invoke the java servlet click <a href="Pixelizator">here</a></p>--%>
 </body>
 </html>

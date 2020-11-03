3cc9b03354d549a188c70d2e1669247d0a9c96d3
asvyryda <asvyryda@student.ucode.world>
2020-11-03 18:34:35 +0200

    Ajax post

--- src/main/java/MyServlet.java
+++ src/main/java/MyServlet.java
@@ -14,6 +14,7 @@ import java.awt.image.Raster;
 import java.awt.image.WritableRaster;
 import java.io.*;
 import java.net.URL;
+import java.nio.charset.StandardCharsets;
 
 @WebServlet(name = "MyServlet")
 @MultipartConfig(fileSizeThreshold=1024*1024*2, // 2MB
@@ -22,72 +23,80 @@ import java.net.URL;
 public class MyServlet extends HttpServlet {
 
     private static final String SAVE_DIR = "uploadFiles";
-    String fileName;
-    String savePath;
 
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         System.out.println("doPost\n\n");
-        // gets absolute path of the web application
-        String appPath = request.getServletContext().getRealPath("");
-        // constructs path of the directory to save uploaded file
-        savePath = appPath + File.separator + SAVE_DIR;
-        // creates the save directory if it does not exists
-        File fileSaveDir = new File(savePath);
-
-        if (!fileSaveDir.exists())
-            fileSaveDir.mkdir();
-        BufferedImage img = null;
-        if (request.getParameter("text").length() == 0) {
-            System.out.println("file\n\n");
-            for (Part part : request.getParts()) {
-                if (extractFileName(part).length() != 0) {
-                    fileName = extractFileName(part);
-                    System.out.println(fileName);
-                    // refines the fileName in case it is an absolute path
-//                    fileName = new File(fileName).getName();
-                    part.write(savePath + File.separator + fileName);
-                    img = ImageIO.read(new File(savePath + File.separator + fileName));
-                }
-            }
-        }
-        else {
-            System.out.println("url\n\n");
-            URL url = new URL(request.getParameter("text"));
-            System.out.println(request.getParameter("text"));
-            img = ImageIO.read(url);
-        }
-        String size = request.getParameter("size");
-        final int PIX_SIZE = Integer.parseInt(size);
-// Read the file as an Image
-//        BufferedImage img = ImageIO.read(new File(savePath + File.separator + fileName));
-// Get the raster data (array of pixels)
-        assert img != null;
-        Raster src = img.getData();
-// Create an identically-sized output raster
-        WritableRaster dest = src.createCompatibleWritableRaster();
-// Loop through every PIX_SIZE pixels, in both x and y directions
-        for(int y = 0; y < src.getHeight(); y += PIX_SIZE) {
-            for(int x = 0; x < src.getWidth(); x += PIX_SIZE) {
-                // Copy the pixel
-                double[] pixel = new double[3];
-                pixel = src.getPixel(x, y, pixel);
-                // "Paste" the pixel onto the surrounding PIX_SIZE by PIX_SIZE neighbors
-                // Also make sure that our loop never goes outside the bounds of the image
-                for(int yd = y; (yd < y + PIX_SIZE) && (yd < dest.getHeight()); yd++) {
-                    for(int xd = x; (xd < x + PIX_SIZE) && (xd < dest.getWidth()); xd++) {
-                        dest.setPixel(xd, yd, pixel);
-                    }
-                }
-            }
-        }
-// Save the raster back to the Image
-        img.setData(dest);
-// Write the new file
-        ImageIO.write(img, "jpg", new File(savePath  + File.separator + "px_"+ fileName));
-        request.setAttribute("path", SAVE_DIR + File.separator + "px_" + fileName);
-        getServletContext().getRequestDispatcher("/index.jsp").forward(
-                request, response);
-//        ImageIO.write(img, "JPG", response.getOutputStream());
+        System.out.println(request.getParameter("file"));
+        System.out.println(request.getParameter("fileUrl"));
+        System.out.println(request.getParameter("pixSize"));
+//        String fileName = "pixel.jpg";
+//        // gets absolute path of the web application
+//        String appPath = request.getServletContext().getRealPath("");
+////        // constructs path of the directory to save uploaded file
+//        String savePath = appPath + File.separator + SAVE_DIR;
+////        // creates the save directory if it does not exists
+//        File fileSaveDir = new File(savePath);
+////
+//        if (!fileSaveDir.exists())
+//            fileSaveDir.mkdir();
+//        BufferedImage img = null;
+//        if (request.getParameter("file").length() != 0) {
+//            System.out.println("file\n\n");
+////            for (Part part : request.getParts()) {
+////                if (extractFileName(part).length() != 0) {
+////                    fileName = extractFileName(part);
+////                    System.out.println(fileName);
+////                    // refines the fileName in case it is an absolute path
+//////                    fileName = new File(fileName).getName();
+////                    part.write(savePath + File.separator + fileName);
+////                    img = ImageIO.read(new File(savePath + File.separator + fileName));
+////                }
+////            }
+//        }
+//        else {
+//            System.out.println("url\n\n");
+//            URL url = new URL(request.getParameter("fileUrl"));
+//            System.out.println(request.getParameter("fileUrl"));
+//            img = ImageIO.read(url);
+//        }
+//
+//        String size = request.getParameter("pixSize");
+//        final int PIX_SIZE = Integer.parseInt(size);
+//// Read the file as an Image
+////        BufferedImage img = ImageIO.read(new File(savePath + File.separator + fileName));
+//// Get the raster data (array of pixels)
+//        assert img != null;
+//        Raster src = img.getData();
+//// Create an identically-sized output raster
+//        WritableRaster dest = src.createCompatibleWritableRaster();
+//// Loop through every PIX_SIZE pixels, in both x and y directions
+//        for(int y = 0; y < src.getHeight(); y += PIX_SIZE) {
+//            for(int x = 0; x < src.getWidth(); x += PIX_SIZE) {
+//                // Copy the pixel
+//                double[] pixel = new double[3];
+//                pixel = src.getPixel(x, y, pixel);
+//                // "Paste" the pixel onto the surrounding PIX_SIZE by PIX_SIZE neighbors
+//                // Also make sure that our loop never goes outside the bounds of the image
+//                for(int yd = y; (yd < y + PIX_SIZE) && (yd < dest.getHeight()); yd++) {
+//                    for(int xd = x; (xd < x + PIX_SIZE) && (xd < dest.getWidth()); xd++) {
+//                        dest.setPixel(xd, yd, pixel);
+//                    }
+//                }
+//            }
+//        }
+//// Save the raster back to the Image
+//        img.setData(dest);
+//// Write the new file
+//        ImageIO.write(img, "jpg", new File(savePath  + File.separator + "px_"+ fileName));
+//
+//        String content = SAVE_DIR + File.separator + "px_" + fileName;
+//        System.out.println(content);
+//        response.setContentType("text/plain");
+//
+//        OutputStream outStream = response.getOutputStream();
+//        outStream.write(content.getBytes(StandardCharsets.UTF_8));
+//        outStream.flush();
+//        outStream.close();
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -11,69 +11,50 @@
     <meta name="description" content="Pixelizator">
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     <title id="name" >Pixelizator</title>
+    <script src="http://code.jquery.com/jquery-latest.min.js"></script>
     <script type="text/javascript" src="https://platform-api.sharethis.com/js/sharethis.js#property=5f9ad1924fa617001994479a&product=image-share-buttons" async="async"></script>
-
 </head>
 <link rel="stylesheet" href="mystyle.css">
 <script>
-    function updateImage() {
-        if ("${path}".length !== 0) {
-            document.getElementById("divImage").style.display = "initial";
-            document.getElementById("downloadButtons").style.display = "initial";
-        }
+    function submitPost() {
+        $.ajax({
+            type: "POST",
+            url : 'Pixelizator',// URL - сервлет
+            data : {                 // передаваемые сервлету данные
+                file : $('#file').val(),
+                fileUrl : $('#fileUrl').val(),
+                pixSize : $('#pixSize').val()
+            },
+            success : function(response) {
+                // обработка ответа от сервера
+                console.log(response);
+                document.getElementById("NewImg").src = response;
+                console.log(document.getElementById("NewImg").src);
+                document.getElementById("downloadButtons").style.display = "initial";
+            }
+        });
     }
-</script>
-<body>
-<h1>Pixelizator</h1>
-<form method="post" enctype="multipart/form-data" action="Pixelizator">
-    <label for="file">Download by file</label>
-    <input name="image" id="file" type="file" accept="image/*">
-    <br>
-    <label for="fileUrl">Download by url</label>
-    <input name="text" id="fileUrl" type='text' />
-    <br>
-    <label for="pixSize">Select size of pixel</label>
-    <input name="size" id="pixSize" type='number' min="0" value="10"/>
-    <br>
-    <div id="divImage" style ="display:none">
-        <button type="submit">Pixelate</button>
-        <br>
-        <img id="Img" src="${path}" class="no-image" alt="your image" onchange="updateImage()">
-        <br>
-        <p>
-            Total size: <span id="fileSize">0 MiB</span>
-        </p>
-    </div>
-</form>
-<div id="downloadButtons" style ="display:none">
-    Download as:
-    <a class="button" href="${path}" download="pixel.jpg">jpg</a>
-    <a class="button" href="${path}" download="pixel.png">png</a>
-    <a class="button" href="${path}" download="pixel.webp">webp</a>
-    <a class="button" href="${path}" download="pixel.bmp">bmp</a>
-</div>
 
-<script>
     window.addEventListener('load', function() {
         document.getElementById("file").addEventListener('change', function() {
             if (this.files && this.files[0]) {
                 var image = document.getElementById("Img");  // $('img')[0]
                 image.src = URL.createObjectURL(this.files[0]); // set src to blob url
-
                 var img= this.files[0].size;
-                var imgsize=img/1024;
+                var imgsize=img/1000000;
                 document.getElementById("fileSize").innerHTML = imgsize.toFixed(3) + " MiB";
                 image.onload = imageIsLoaded;
                 document.getElementById("divImage").style.display = "initial";
             }
         });
         document.getElementById("fileUrl").addEventListener('change', function() {
-                var image = document.getElementById("Img");  // $('img')[0]
-                var downloadingImage = new Image();
-                downloadingImage.onload = function(){
-                    image.src = this.src;
-                };
-                downloadingImage.src = document.getElementById("fileUrl").value;
+            var image = document.getElementById("Img");  // $('img')[0]
+            var downloadingImage = new Image();
+            downloadingImage.onload = function(){
+                image.src = this.src;
+            };
+            downloadingImage.src = document.getElementById("fileUrl").value;
+            document.getElementById("divImage").style.display = "initial";
 
             // var img = downloadingImage.files[0].size;
             // var imgsize=img/1024;
@@ -92,5 +73,35 @@
         // update width and height ...
     }
 </script>
+<body>
+<h1>Pixelizator</h1>
+<%--<form method="post" enctype="multipart/form-data" action="Pixelizator">--%>
+    <label for="file">Download by file</label>
+    <input name="image" id="file" type="file" accept="image/*">
+    <br>
+    <label for="fileUrl">Download by url</label>
+    <input name="text" id="fileUrl" type='text' />
+    <br>
+    <label for="pixSize">Select size of pixel</label>
+    <input name="size" id="pixSize" type='number' min="0" value="10"/>
+    <br>
+    <div id="divImage" style ="display:none">
+        <button id="submit" onclick="submitPost()" >Pixelate</button>
+        <br>
+        <img id="Img" src="" class="no-image" alt="your image">
+        <br>
+        <p>
+            Total size: <span id="fileSize">0 MiB</span>
+        </p>
+    </div>
+<%--</form>--%>
+<div id="downloadButtons" style ="display:none">
+    <img id="NewImg" src="" class="no-image" alt="your image">
+    Download as:
+    <a class="button" href="${path}" download="pixel.jpg">jpg</a>
+    <a class="button" href="${path}" download="pixel.png">png</a>
+    <a class="button" href="${path}" download="pixel.webp">webp</a>
+    <a class="button" href="${path}" download="pixel.bmp">bmp</a>
+</div>
 </body>
 </html>
